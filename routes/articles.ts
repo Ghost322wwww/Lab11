0e64952a2ba67f753c023ab471ca82aa99d7f466
Ghost322wwww <kwokc289@yahoo.com>
2025-05-12 22:32:06 +0800

    Create articles.ts

---
+++ routes/articles.ts
@@ -0,0 +1,31 @@
+import * as model from '../models/articles';
+
+const getAll = async (ctx: RouterContext, next: any) => {
+  let articles = await model.getAll();
+  ctx.body = articles.length ? articles : {};
+  await next();
+}
+
+const getById = async (ctx: RouterContext, next: any) => {
+  let id = ctx.params.id;
+  let article = await model.getById(id);
+  if (article.length) {
+    ctx.body = article[0];
+  } else {
+    ctx.status = 404;
+  }
+  await next();
+}
+
+const createArticle = async (ctx: RouterContext, next: any) => {
+  const body = ctx.request.body;
+  let result = await model.add(body);
+  if (result.status === 201) {
+    ctx.status = 201;
+    ctx.body = body;
+  } else {
+    ctx.status = 500;
+    ctx.body = { err: "insert data failed" };
+  }
+  await next();
+}
