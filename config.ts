374f91bfc05a6dfea77e0fe6dafb89aa2d022d55
Ghost322wwww <kwokc289@yahoo.com>
2025-05-12 22:29:33 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,6 +2,7 @@ export const config = {
   host: 'localhost',      
   port: 5432,            
   user: 'postgres',        
+  password: '',   
   database: 'postgres',    
   connection_limit: 100
 }
