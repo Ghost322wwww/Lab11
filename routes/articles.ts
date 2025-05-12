6a464de6036c6b08f801dded99e51daf2a847692
Ghost322wwww <kwokc289@yahoo.com>
2025-05-12 22:40:05 +0800

    FIX

--- routes/articles.ts
+++ routes/articles.ts
@@ -1,13 +1,13 @@
 import { RouterContext } from '@koa/router';
 import * as model from '../models/articles';
 
-const getAll = async (ctx: RouterContext, next: any) => {
+export const getAll = async (ctx: RouterContext, next: any) => {
   let articles = await model.getAll();
   ctx.body = articles.length ? articles : {};
   await next();
 }
 
-const getById = async (ctx: RouterContext, next: any) => {
+export const getById = async (ctx: RouterContext, next: any) => {
   let id = ctx.params.id;
   let article = await model.getById(id);
   if (article.length) {
@@ -18,8 +18,8 @@ const getById = async (ctx: RouterContext, next: any) => {
   await next();
 }
 
-const createArticle = async (ctx: RouterContext, next: any) => {
-  const body = ctx.request.body;
+export const createArticle = async (ctx: RouterContext, next: any) => {
+  const body = (ctx.request as any).body; 
   let result = await model.add(body);
   if (result.status === 201) {
     ctx.status = 201;
