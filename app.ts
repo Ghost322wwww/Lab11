8e75a3820844fe7bbe1df9c6ff87807709c0c014
Ghost322wwww <kwokc289@yahoo.com>
2025-05-12 23:17:19 +0800

    fix

--- app.ts
+++ app.ts
@@ -1,22 +1,17 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
-import Router from '@koa/router';
-import articleRoutes from './routes/articles';
+import router from './routes/articles'; 
 
 const app = new Koa();
-const router = new Router();
 
 app.use(bodyParser());
 
-// 路由設定
-router.get('/articles', articleRoutes.getAll);
-router.get('/articles/:id', articleRoutes.getById);  
-router.post('/articles', articleRoutes.createArticle);
-
+// 加入路由
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// 啟動伺服器
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
