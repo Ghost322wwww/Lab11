8dd02ac4e854615912d3b6433400c6fa0a900601
Ghost322wwww <kwokc289@yahoo.com>
2025-05-17 21:57:52 +0800

    Update special.ts

--- routes/special.ts
+++ routes/special.ts
@@ -1,15 +1,21 @@
-import Router, { RouterContext } from "@koa/router";
+import Router, { RouterContext } from '@koa/router';
 import { basicAuth } from '../controllers/auth';
 
 const router = new Router({ prefix: '/api/v1' });
 
-router.get('/', async (ctx: RouterContext, next: any) => {
+router.get('/', async (ctx: RouterContext, next) => {
   ctx.body = {
-    message: 'Public API return'
+    message: 'Public API return',
   };
   await next();
 });
 
-router.get('/private', basicAuth);
+router.get('/private', basicAuth, async (ctx: RouterContext, next) => {
+  ctx.body = {
+    message: 'This is a protected route!',
+    user: ctx.state.user || 'unknown',
+  };
+  await next();
+});
 
 export { router };
